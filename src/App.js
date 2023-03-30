8fd6e20fe392026ecc46b4c6a757a4b100747863
seokmin <essemfly@gmail.com>
2023-03-30 13:58:58 +0900

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import Product from "./Product";
 
-function App() {
+const App = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch(
+        "http://52.79.251.236/danggns?offset=0&limit=100&keyword=직구"
+      );
+      const data = await response.json();
+      setProducts(data);
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>해외직구 반품 상품들</h1>
+      <div className="product-list">
+        {products.map((product) => {
+          return (
+            <Product
+              key={product.id}
+              images={product.images !== null ? product.images : []}
+              category={product.crawl_category}
+              name={product.name}
+              price={product.price}
+              description={product.description}
+            />
+          );
+        })}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Product.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "react-multi-carousel/lib/styles.css";
+
+const Product = ({ images, category, name, price, description }) => {
+  let image = images[0];
+  return (
+    <div className="product">
+      <img className="product-image" src={image} alt={name} key={image} />
+      <div className="product-details">
+        <div className="product-category">{category}</div>
+        <div className="product-name">{name}</div>
+        <div className="product-price">{price}</div>
+        <div className="product-desc">{description}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Product;
